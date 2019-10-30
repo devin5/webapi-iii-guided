a62173c9cff21bd8a7f4deff33c10e7ba46c14ad
Devin Dias <devindias.tech@gmail.com>
2019-10-30 12:05:32 -0500

    custom middleware

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-const express = require('express'); // importing a CommonJS module
-
-const hubsRouter = require('./hubs/hubs-router.js');
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
-server.use(express.json());
+server.use("/api/hubs", hubsRouter);
 
-server.use('/api/hubs', hubsRouter);
+function dateLogger(req, res, next) {
+  console.log(Date.now.toISOString());
+  next();
+}
+function Logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+  next();
+}
+// global
+server.use(helmet()); //third party
+server.use(express.json()); // built in
+server.use(dateLogger);
+server.use(Logger);
+
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
